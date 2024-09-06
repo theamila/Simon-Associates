acac5c2ee8e3aa01f5e4753cfe881bd33591f2a0
Anushka-OSI <anushkapiyumalmrc@gmail.com>
2024-09-06 15:46:30 +0530

    0.0.26

--- database/migrations/2024_03_21_060927_create_invoice_details_table.php
+++ database/migrations/2024_03_21_060927_create_invoice_details_table.php
@@ -6,9 +6,11 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
         Schema::create('invoice_details', function (Blueprint $table) {
